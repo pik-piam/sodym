29a2765a0f53bab96706333a9451e9d8c124b7dd
Sally Dacie <sallyda@LT1189.pik-potsdam.de>
2024-08-06 16:14:33 +0200

    Rename definition classes

--- sodym/classes/mfa_definition.py
+++ sodym/classes/mfa_definition.py
@@ -1,40 +1,40 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel as PydanticBaseModel, Field, field_validator
 from typing import List, Optional
 
 
-class DimensionDefinition(BaseModel):
+class DimensionDefinition(PydanticBaseModel):
     name: str = Field(..., min_length=2)
     dim_letter: str = Field(..., min_length=1, max_length=1)
     filename: Optional[str] = None
     dtype: type
 
 
-class SomethingWithDimensions(BaseModel):
+class DefinitionWithDimLetters(PydanticBaseModel):
     dim_letters: tuple
 
     @field_validator("dim_letters", mode='before')
     def check_dimensions(cls, v):
         for letter in v:
             if (not isinstance(letter, str)) or (len(letter) != 1):
-                raise ValueError('flows must be defined using single digit dimension letters')
+                raise ValueError('Dimensions must be defined using single digit letters')
         return v
 
 
-class FlowDefinition(SomethingWithDimensions):
+class FlowDefinition(DefinitionWithDimLetters):
     from_process: str
     to_process: str
 
 
-class StockDefinition(SomethingWithDimensions):
+class StockDefinition(DefinitionWithDimLetters):
     name: Optional[str] = None
     process: Optional[str] = None
 
 
-class ParameterDefinition(SomethingWithDimensions):
+class ParameterDefinition(DefinitionWithDimLetters):
     name: str
 
 
-class MFADefinition(BaseModel):
+class MFADefinition(PydanticBaseModel):
     """
     All the information needed to define an MFA system.
     """
