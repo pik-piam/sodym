b06aeea5fe5153f6ffdc029ff3321b7ffe54b77d
Jakob Duerrwaechter <jakob.duerrwaechter@pik-potsdam.de>
2024-09-24 11:57:50 +0200

    use DimensionSet.expand_by in nda stack

--- sodym/named_dim_array_helper.py
+++ sodym/named_dim_array_helper.py
@@ -8,7 +8,7 @@ def named_dim_array_stack(named_dim_arrays: list[NamedDimArray], dimension: Dime
     Method can be applied to `NamedDimArray`s, `StockArray`s, `Parameter`s and `Flow`s.
     """
     named_dim_array0 = named_dim_arrays[0]
-    extended_dimensions = DimensionSet(dimensions=named_dim_array0.dims.dimensions+[dimension])
+    extended_dimensions = named_dim_array0.dims.expand_by([dimension])
     extended = NamedDimArray(dims=extended_dimensions)
     for item, nda in zip(dimension.items, named_dim_arrays):
         extended[{dimension.letter: item}] = nda
